149a37da97bf8c17f7c67dc4ede9660296cbb83d
david stern <dzs101010@gmail.com>
2026-06-24 12:23:58 +0300

    add loadfile and foundfile functions

--- Program.cs
+++ Program.cs
@@ -1,2 +1,48 @@
-﻿using System.IO;
-namespace FileInfo;
+﻿using System;
+using System.IO;
+namespace Demo;
+class Test
+{
+    static void Main()
+    {
+        
+        string FileName = "reports.txt";
+        LoadFile(FoundPath(FileName), FileName);
+
+    }
+    static string FoundPath(string filename)
+    {
+        string baseDir = AppDomain.CurrentDomain.BaseDirectory;
+        DirectoryInfo projectDir = Directory.GetParent(baseDir).Parent.Parent.Parent;
+        string FullPath = Path.Combine(projectDir.FullName, filename);
+        return FullPath;
+    }
+    static int?[] LoadFile(string FullPath, string FileName)
+    { 
+        if (File.Exists(FullPath))
+        {
+            string[] lines = File.ReadAllLines(FullPath);
+            int lineCount = lines.Length;
+            if (lineCount > 0)
+            {
+                int?[] lineCountArrey = new int?[lineCount];
+                Console.WriteLine($"File loaded: {lineCount} lines found.");
+                return lineCountArrey;
+            }
+            else
+            {
+                int?[] emptyFile = null;
+                Console.WriteLine("Error: File is empty.");
+                return emptyFile;
+            }       
+        }
+        else
+        {
+            int?[] notFound = null;
+            Console.WriteLine($"Error: File '{FileName}' not found.");
+            return notFound;
+        }
+    }
+    
+    
+}
